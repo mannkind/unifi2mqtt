19b5e52c7ca6a34f07fb59f943dc4b743700f435
Dustin Brewer <mannkind@thenullpointer.net>
2021-11-01 19:55:43 -0700

    Fix test

--- UnifiTest/Liasons/MQTTLiasonTest.cs
+++ UnifiTest/Liasons/MQTTLiasonTest.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using TwoMQTT.Utils;
+using TwoMQTT.Interfaces;
 using Unifi.Liasons;
 using Unifi.Models.Options;
 using Unifi.Models.Shared;
